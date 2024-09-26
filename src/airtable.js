c13e981add3c5d4e1e75b298cecf2e962597af82
Adam Barr-Neuwirth <slugiscool99@users.noreply.github.com>
2024-09-25 21:24:01 -0400

    working downloading from airtable

--- src/airtable.js
+++ src/airtable.js
@@ -1,27 +1,43 @@
 require("dotenv").config()
 
 const token = process.env.AIRTABLE_TOKEN || ""
-const baseId = process.env.AIRTABLE_BASE_ID || ""
-const tableName = process.env.AIRTABLE_TABLE_NAME || ""
 
-const downloadData = async (formula = "") => {
+const downloadData = async (url = "") => {
   const requestOptions = {
     headers: {
       Authorization: "Bearer " + token,
     },
   }
-
-  const response = await fetch(
-    "https://api.airtable.com/v0/" +
-      baseId +
-      "/" +
-      tableName +
-      "?filterByFormula=" +
-      formula,
-    requestOptions
-  )
-  const data = await response.text()
+  const response = await fetch(url, requestOptions)
+  const data = await response.json()
   console.log(data)
+  return data
 }
 
-downloadData()
+const getBaseAndTable = (client) => {
+  if (client == "mro") {
+    return {
+      baseId: "apprCx0TVcBxkX4Vm",
+      tableName: "tblule8kefnyEGVB6",
+    }
+  } else if (client == "gruns") {
+    return {
+      baseId: "appyR6tuVD0RO2dls",
+      tableName: "tblBANBVeGMS8L41c",
+    }
+  }
+}
+
+const getLatestRecords = async (clientName, limit = 10) => {
+  const { baseId, tableName } = getBaseAndTable(clientName)
+  const formula = `AND(RECORD_ID() != '', CREATED_TIME() != '', {Slides Drive URL} != '')` // Ensure we get valid records
+  const url = `https://api.airtable.com/v0/${baseId}/${tableName}?maxRecords=${limit}&filterByFormula=${encodeURIComponent(formula)}`
+
+  const data = await downloadData(url)
+
+  return data.records
+}
+
+module.exports = {
+  getLatestRecords,
+}
---
+++ src/main.js
@@ -0,0 +1,81 @@
+require("dotenv").config()
+const fs = require("fs")
+const path = require("path")
+
+const { authorize, downloadFile, getFolderContent } = require("./gdrive")
+const { getLatestRecords } = require("./airtable")
+
+const extractFileIdFromUrl = (url) => {
+  const fileMatch = url.match(/\/d\/([a-zA-Z0-9-_]+)/)
+  if (fileMatch) return fileMatch[1]
+
+  const folderMatch = url.match(/\/folders\/([a-zA-Z0-9-_]+)/)
+  return folderMatch ? folderMatch[1] : null
+}
+
+const saveInfoAsJson = (files, record, folderPath) => {
+  const taskObject = {
+    files: files,
+    caption: record.fields["Caption"],
+    sound: record.fields["Sound"],
+    account: record.fields["TikTok @handle"],
+    device: record.fields["Device"],
+  }
+
+  const filePath = path.join(folderPath, "slideshow_" + record.id + ".json")
+  fs.writeFile(filePath, JSON.stringify(taskObject, null, 2), (err) => {
+    if (err) {
+      console.error("Error saving files to JSON:", err)
+      return
+    }
+    console.log("Files saved to JSON successfully.")
+  })
+}
+
+const downloadLatestImagesAndSaveToJson = async (clientName) => {
+  try {
+    const auth = await authorize()
+    const records = await getLatestRecords(clientName)
+    var files = []
+    for (const record of [records[0]]) {
+      const driveUrl = record.fields["Slides Drive URL"]
+      if (driveUrl) {
+        const folderId = extractFileIdFromUrl(driveUrl)
+        if (folderId) {
+          const folderContents = await getFolderContent(auth, folderId)
+          const recordFolder = path.join(__dirname, record.id)
+          if (!fs.existsSync(recordFolder)) {
+            fs.mkdirSync(recordFolder, { recursive: true })
+          }
+
+          for (const file of folderContents) {
+            const fileName = `${file.name}`
+            const filePath = path.join(recordFolder, fileName)
+            await downloadFile(auth, file.id, filePath)
+            files.push(fileName)
+          }
+          saveInfoAsJson(files, record, recordFolder)
+        } else {
+          console.log(`Invalid Drive URL for record ${record.id}`)
+        }
+      } else {
+        console.log(`No Drive URL found for record ${record.id}`)
+      }
+    }
+  } catch (error) {
+    console.error("Error downloading images:", error)
+  }
+}
+
+function main(clientName) {
+  if (!clientName) {
+    console.error("Usage: node main.js <clientName> (gruns or mro)")
+    process.exit(1)
+  }
+  downloadLatestImagesAndSaveToJson(clientName)
+}
+
+const args = process.argv.slice(2)
+const clientName = args[0]
+
+main(clientName)
